11b5c808017c965b47100755a03afccb96f7ade3
MaakuW <maakuwemail@gmail.com>
2021-08-12 22:43:50 -0400

    Cleaned out the Tooltips, decreasing the load and fixing usability
    Tooltips on such a small bar were not necessary, and hindered interaction with the Header

--- resources/js/global/Topbar.js
+++ resources/js/global/Topbar.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import cms from '../cms.json'
-import {Avatar, Tooltip} from 'antd'
+import {Avatar} from 'antd'
 import { getPaths } from '../functions'
 
 class Topbar extends Component {
@@ -12,32 +12,25 @@ class Topbar extends Component {
       <nav data-topbar>
         <div className="d-flex h-100 justify-content-between align-items-center">
           <div className="d-flex col-8">
-            <a href={is_admin ? "/" : "/admin"} className="d-block me-1">
-              <Tooltip title={`Jump to the ${is_admin ? 'touchscreen' : 'content managment system Dashboard'}`}>
-                <svg className="icon">
-                  <use xlinkHref="#logo__bluewater" style={{width: '1.25rem', height: '1.25rem'}}/>
-                </svg>
-                <span className="ms-1">Bluewater {is_admin ? 'Touchscreen' : 'CMS'}</span>
-              </Tooltip>
+            <a href={is_admin ? "/" : "/admin"} className="d-block me-1" title={`Jump to the ${is_admin ? 'touchscreen' : 'content managment system Dashboard'}`}>
+              <svg className="icon">
+                <use xlinkHref="#logo__bluewater" style={{width: '1.25rem', height: '1.25rem'}}/>
+              </svg>
+              <span className="ms-1">Bluewater {is_admin ? 'Touchscreen' : 'CMS'}</span>
             </a>
-            <a href="/logout"
-            className="d-block mx-1">
-              <Tooltip title="Logout">
-                <svg className="icon">
-                  <use xlinkHref="#icon__logout" style={{width: '1.25rem', height: '1.25rem'}}/>
-                </svg>
-                <span className="ms-1">Logout</span>
-              </Tooltip>
+            <a href="/logout" className="d-block mx-1" title="Logout">
+              <svg className="icon">
+                <use xlinkHref="#icon__logout" style={{width: '1.25rem', height: '1.25rem'}}/>
+              </svg>
+              <span className="ms-1">Logout</span>
             </a>
             {this.props.user_type === 1 &&
             <a href={`https://console.firebase.google.com/u/1/project/${cms.settings.firebase.project_id}/storage/${cms.settings.firebase.storage_bucket}/files`}
-            className="d-block mx-1" target="_blank">
-              <Tooltip title="View my Google Firebase Account">
-                <svg className="icon">
-                  <use xlinkHref="#logo__firebase" style={{width: '1.25rem', height: '1.25rem'}}/>
-                </svg>
-                <span className="ms-1">Firebase</span>
-              </Tooltip>
+            className="d-block mx-1" target="_blank" title="View my Google Firebase Account">
+              <svg className="icon">
+                <use xlinkHref="#logo__firebase" style={{width: '1.25rem', height: '1.25rem'}}/>
+              </svg>
+              <span className="ms-1">Firebase</span>
             </a>
             }
           </div>
